9fed58846226ec13525114308136d3c82589f900
Mackenzie Lach <kenzie@Mackenzies-MacBook-Pro.local>
2023-07-12 15:37:43 -0700

    added Keck aperture

--- masks_functs.py
+++ masks_functs.py
@@ -1,22 +1,59 @@
 import numpy as np
 
 def check_placement(coords, design, rng, aperture):
-    # check to make sure hole falls onto a mirror segment and doesn't overlap a spider or another hole
-    # uses a boolean mask of the aperture and holes already placed
-    # if there's a problem, calls add_hole() again
-    check = 
-    if check == 1:
-        coords = add_hole(design.hrad, rng, aperture)
+    """ Check placement of hole
+
+    Called by add_hole(). Checks that a proposed hole doesn't overlap other holes or spiders or mirror segment edges, and that it falls within the Keck aperture. If a hole does not meet requirements, hole is discarded and add_hole() is called again. Repeats until an acceptable hole location is found.
+
+    Args:
+        coords (array): numpy array containing the proposed (x,y) coordinates. 
+        design (object): Mask design object.
+        rng (object): Random number generator.
+        aperture (array): numpy array containing a boolean mask of the Keck primary.
+    
+    Returns:
+        numpy array: returns the accepted (x,y) coordinates
+    """
+    [ap_x, ap_y] = 100 * coords + [545, 545] # convert proposed hole center coords to coords in aperture array
+    for i in range(1090):
+        for j in range(1090):
+            if (np.sqrt((i - ap_y)**2 + (j - ap_x)**2) < design.hrad) and (aperture[i, j] == 0):
+                new_coords = add_hole(design.hrad, rng, aperture)
+                return new_coords
+            else:
+                return coords
 
 def add_hole(hrad, rng, aperture):
-    # generates coordinates for a new hole, checks that hole doesn't intersect spiders or other holes and falls within aperture
+    """ Propose a new mask hole
+
+    Generates a proposed hole (x,y) coordinate set, then calls check_placement() to check whether these coordinates are acceptable. If they are, then these coordinates are returned. 
+    
+    Args:
+        hrad (float): Hole radius in meters.
+        rng (object): Random number generator.
+        aperture (array): numpy array containing a boolean mask of the Keck primary.
+    
+    Returns:
+        array: Returns a numpy array of the accepted hole (x,y) coordinates.
+    """
+
     rand_nums = rng.integer(low=-1, high=1, size=2, dtype=float)
     coords = rand_nums * 11
-    check_placement(coords, hrad, rng, aperture)
+    coords = check_placement(coords, hrad, rng, aperture)
     return coords
 
 def check_redundancy(my_design):
-    # checks that baselines are 0% redundant
+    """ Check mask baselines for redundancy
+
+    Checks for any redundancy in the baselines of the proposed mask design. If redundancy is above 0%, reject the mask design.
+
+    Args:
+        my_design (object): An instance of design class. The proposed mask to be tested.
+
+    Returns:
+        bool: Returns 1 if the mask has any redundancy, returns 0 if mask is fully non-redundant.
+    """
+
     uv_rad = my_design.hrad
     n = 50000
     for i in my_design.uv_coords:
