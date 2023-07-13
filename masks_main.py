1038b950ccf34408709b3736633ed9085a83251a
Mackenzie Lach <kenzie@Mackenzies-MacBook-Pro.local>
2023-07-13 15:31:14 -0700

    fixed a lot of things, code is working but very slow!

--- masks_main.py
+++ masks_main.py
@@ -7,16 +7,17 @@ def main(nholes, hrad):
     while 1: # keep looping until we get a valid design
         my_design = design(nholes, hrad) # initialize design object
         rng = np.random.default_rng(seed=None) # set random number generator
-        xy_coords = np.empty([nholes, 2]) # initialize array to hold x,y coordinates
         aperture = pyfits.getdata('/Users/kenzie/Desktop/CodeAstro/planet-guts/keck_aperture.fits') # set Keck primary aperture
         for i in range(nholes): # keep adding and checking a single hole until it's acceptable
-            xy_coords[i] = add_hole(hrad, rng, aperture)
-        my_design.xy_coords = xy_coords # once we get enough acceptable holes, add them to design
+            my_design.xy_coords[i] = add_hole(hrad, rng, aperture)
 
         my_design.get_uvs() # calculate design uv coordinates
 
         rcheck = check_redundancy(my_design)  # check design for redundancy
-        if rcheck: # if true, there's some redundancy and we need to start over
+        if rcheck == 1: # if true, there's some redundancy and we need to start over
             print("Uh-oh, mask has redundancies! Trying again...")
         if rcheck == 0: # if this statement is true, exit the loop and return our final design!
+            print("Yay! Mask design is non-redundant. Plotting design...")
+            #print(my_design.xy_coords)
+            plot_design(my_design, aperture)
             return my_design
