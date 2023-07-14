ee8273333728bd2d0ecbfe4aff88b6a080205134
Mackenzie Lach <kenzie@Mackenzies-MacBook-Pro.local>
2023-07-13 19:15:26 -0700

    edited main code to run individual functions

--- exoplanet_masses.py
+++ exoplanet_masses.py
@@ -1,6 +1,25 @@
 # Main code for code astro project
 # Kenzie, Loraine, and Helena
+from main import *
+import massive_galaxies
 
-print('hi')
+print('Hello, welcome to the multidisciplinary package planet-guts! Please select your discipline; enter 0 for interferometric masking, 1 for galaxy catalogs, and 2 for numerical integration:')
 
-print('hello hellooo')
+in_val = input()
+
+if in_val != (0 or 1 or 2):
+    print("Oops! Input must be 0, 1, or 2.")
+
+if in_val == 0:
+    print("You've selected masking mode! Please enter number of holes:")
+    nholes = int(input())
+    print("Thanks! Next, please enter the hole diameter in meters:")
+    hrad = float(input())
+    print('Thanks! Making a mask...')
+    main(nholes, hrad)
+
+if in_val == 1:
+    print("You've selected galaxy mode!")
+
+if in_val == 2:
+    print("You've selected numerical integration mode!")
